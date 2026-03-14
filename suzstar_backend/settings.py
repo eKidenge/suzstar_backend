e5b4b4cdc7fdd98cbe0af5da134d4243564dd967
Elisha Kidenge <elishakidenge@gmail.com>
2026-03-14 21:08:14 +0300

    Add Gunicorn to requirements for Render deployment

--- suzstar_backend/settings.py
+++ suzstar_backend/settings.py
@@ -129,7 +129,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:5173",  # Vite default port
     "http://127.0.0.1:5173",
-    'https://suzstar-backend.onrender.com',
+    "https://suzstar-backend.onrender.com",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
